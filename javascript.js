a12e3491569ebb00381fb6afaaa7e7221e9158d9
Jennifer Amanze <jennfranc18@gmail.com>
2025-09-04 13:53:15 +0100

    JS Website

---
+++ javascript.js
@@ -0,0 +1,8 @@
+const name = prompt("What is your name?")
+console.log(name)
+
+let jobTitle = prompt("What is your job role?")
+console.log(jobTitle)
+
+const amHuman = confirm("Are you human?")
+console.log(amHuman)
