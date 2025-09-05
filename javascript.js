8138101ea281a74dd8a497def6a0b57d5d6a1b65
Jennifer Amanze <jennfranc18@gmail.com>
2025-09-05 23:29:15 +0100

    Assignment II Submission

--- javascript.js
+++ javascript.js
@@ -8,9 +8,9 @@ let subtraction = num1 - num2;
 let multiplication = num1 * num2;
 let division = num1 / num2;
 
-console.log("Addition")
-console.log("Subtraction")
-console.log("Multiplication")
-console.log("Division")
+console.log("Addition:", addition)
+console.log("Subtraction:", subtraction)
+console.log("Multiplication:", multiplication)
+console.log("Division:", division)
 
 console.log(`Hello ${name}, the result of num1 + num2 is ${addition}`);
