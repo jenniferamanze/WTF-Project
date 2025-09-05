d2b5bc22f05344238a8ad487e9e95b11d6f9a5f2
Jennifer Amanze <jennfranc18@gmail.com>
2025-09-05 23:23:29 +0100

    Assignment II Submission

--- javascript.js
+++ javascript.js
@@ -1,8 +1,16 @@
-const name = prompt("What is your name?")
-console.log(name)
 
-let jobTitle = prompt("What is your job role?")
-console.log(jobTitle)
+let num1 = 20;
+let num2 = 30;
+let name = "Jennifer";
 
-const amHuman = confirm("Are you human?")
-console.log(amHuman)
+let addition = num1 + num2;
+let subtraction = num1 - num2;
+let multiplication = num1 * num2;
+let division = num1 / num2;
+
+console.log("Addition")
+console.log("Subtraction")
+console.log("Multiplication")
+console.log("Division")
+
+console.log(`Hello ${name}, the result of num1 + num2 is ${addition}`);
